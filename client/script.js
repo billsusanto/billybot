ba1d0cc49d65af0a067c5471de66215ae3458731
billsusanto <billsusanto01@gmail.com>
2023-09-09 00:01:21 -0700

    Update server url

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://billybot.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
